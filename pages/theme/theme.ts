7921fde48a052f781447d3c098f7c89a4e31321e
Anna Filatova <filatovaa@yahoo.com>
2022-10-24 00:16:36 -0700

    adding theme

---
+++ pages/theme/theme.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme(
+    {
+        colors:{
+            balck:'black',
+            white:'white'
+        }
+    }
+)
+
+export default theme
