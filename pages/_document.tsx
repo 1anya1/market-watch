20b7cce5d581d18b86c9540935b65ab65d1086b6
Anna Filatova <filatovaa@yahoo.com>
2022-10-28 23:49:03 -0700

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import { Html, Head, Main, NextScript } from "next/document";
-import theme from "theme/theme";
+import theme from "./theme/theme"
 
 export default function Document() {
   return (
