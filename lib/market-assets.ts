b9efecfdda50f68b9e0b72ab3cd40b4d3cb92d05
Anna Filatova <filatovaa@yahoo.com>
2022-11-09 20:39:18 -0800

    update

--- lib/market-assets.ts
+++ lib/market-assets.ts
@@ -3,10 +3,19 @@ import { getJsonFile, uploadJsonFile } from "./s3";
 const fs = require("fs");
 
 export async function updateCards() {
-  const me = { name: "Dante", dog: "cat" };
-  if (me) {
-    uploadJsonFile(me, "test", "card");
-    return true;
+  const getData = await getJsonFile("news", "feed");
+  if (await getData) {
+    return getData;
+  } else {
+    const data = await fetch(
+      `https://newsapi.org/v2/everything?q=crypto&from=2022-11-08&sortBy=popularity&apiKey=${process.env.NEWS_KEY}&language=en`
+    )
+      .then((response) => response.json())
+      .then((data) => data);
+    if ((await data.status) === "ok") {
+      uploadJsonFile(await data, "news", "feed");
+      return true;
+    }
   }
   return false;
 }
--- pages/api/newsfeed.ts
+++ pages/api/newsfeed.ts
@@ -1,11 +1,11 @@
 import { updateCards } from "../../lib/market-assets";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Result = { result: boolean }
+type Result = { result: any };
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Result>,
+  res: NextApiResponse<Result>
 ) {
   res.status(200).json({ result: await updateCards() });
 }
