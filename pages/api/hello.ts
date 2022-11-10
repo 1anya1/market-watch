dc4c13be8cebf34f6d0ef6b375f0cc9500fbeecb
Anna Filatova <filatovaa@yahoo.com>
2022-11-09 20:05:48 -0800

    update

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -17,7 +17,7 @@ export default async function handler(
   res: NextApiResponse<any>
 ) {
   const data = await fetch(
-    `https://newsapi.org/v2/everything?q=crypto&from=2022-11-08&sortBy=popularity&apiKey=${process.env.NEWS_API}&language=en`
+    `https://newsapi.org/v2/everything?q=crypto&from=2022-11-08&sortBy=popularity&apiKey=${process.env.NEWS_KEY}&language=en`
   ).then((response) => response.json());
 
   res.json(data);
