6e98e22cbeef21aca1867f2b3354d92582689d6c
Anna Filatova <filatovaa@yahoo.com>
2022-11-19 00:53:07 -0800

    update

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 import { RiSettings3Fill } from "react-icons/ri";
 import { NumericFormat } from "react-number-format";
+import { BiLinkExternal } from "react-icons/bi";
 import {
   Box,
   Button,
@@ -14,6 +15,14 @@ import {
   Text,
   useColorMode,
   Image,
+  Stack,
+  VStack,
+  Link,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  TabPanels,
 } from "@chakra-ui/react";
 import Bitcoin from "../white/btc.svg";
 import BitcoinLightMode from "../black/btc.svg";
@@ -38,20 +47,28 @@ const ChartComponent = (props: any) => {
   const [twentyFourHourValue, setTwentyFourHourValue] = useState(0);
   const [timeFrameMax, setTimeFrameMax] = useState(0);
   const [timeFrameLow, setTimeFrameLow] = useState(0);
+  const [cryptoId, setCryptoId] = useState(0);
+  const [viewAllCoin, setViewAllCoin] = useState(false);
+  const [news, setNews] = useState<any>({
+    articles: [],
+    page: 0,
+    size: 0,
+    videos: [],
+    total: 0,
+  });
+
   const [coinInfo, setCoinInfo] = useState({
     name: "",
     description: "",
     url: "",
     image: "",
     score: 0,
+    symbol: "",
   });
   const { colorMode } = useColorMode();
 
   useEffect(() => {
     const getData = async () => {
-      // const date = new Date();
-      // const to = Math.floor(date.getTime() / 1000);
-      // const from = (date.getTime() - timeFrame * 24 * 60 * 60 * 1000) / 1000;
       let crypto: any[] = [];
       let low = Infinity;
       let high = 0;
@@ -60,20 +77,7 @@ const ChartComponent = (props: any) => {
           `https://api.coingecko.com/api/v3/coins/${coinId}/ohlc?vs_currency=usd&days=${timeFrame}`
         ),
         await fetch(
-          `https://api.coingecko.com/api/v3/coins/${coinId}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false`
-        ),
-      ])
-        .then(async ([respOne, respTwo]) => [
-          await respOne.json(),
-          await respTwo.json(),
-        ])
-        .then(([graph, data]) => console.log({ graph }, { data }));
-      Promise.all([
-        await fetch(
-          `https://api.coingecko.com/api/v3/coins/${coinId}/ohlc?vs_currency=usd&days=${timeFrame}`
-        ),
-        await fetch(
-          `https://api.coingecko.com/api/v3/coins/${coinId}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false`
+          `https://api.coingecko.com/api/v3/coins/${coinId}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`
         ),
       ])
         .then(async ([respOne, respTwo]) => [
@@ -81,6 +85,7 @@ const ChartComponent = (props: any) => {
           await respTwo.json(),
         ])
         .then(([graph, data]) => {
+          console.log({ data }, { graph });
           graph.forEach((el: any) => {
             const frame = {
               value: el[1],
@@ -101,67 +106,50 @@ const ChartComponent = (props: any) => {
 
             image: data?.image?.small,
             score: data?.community_score,
+            symbol: data?.symbol,
           };
+          console.log(
+            data.market_data.high_24h.usd,
+            data.market_data.low_24h.usd
+          );
+
           setCoinInfo(coinInfo);
           setData(crypto);
           setCurrentValue(crypto[crypto.length - 1].open);
-          console.log(crypto[0].value, crypto[1].value);
+          console.log();
           setTwentyFourHourValue(crypto[0].open);
           setTimeFrameMax(high);
           setTimeFrameLow(low);
         });
-
-      //   const bitcoinData = await fetch(
-      //     // `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=${from}&to=${to}`
-      //     `https://api.coingecko.com/api/v3/coins/${coinId}/ohlc?vs_currency=usd&days=${timeFrame}`
-      //   );
-      //   if (bitcoinData.ok) {
-      //     const resData = await bitcoinData.json();
-      //     // const from =
-      //     //   Math.floor(new Date().getTime() - 24 * 60 * 60 * 1000) / 1000;
-
-      //     resData.forEach((el: any) => {
-      //       const frame = {
-      //         value: el[1],
-      //         time: el[0] / 1000,
-      //         open: el[1],
-      //         close: el[4],
-      //         high: el[2],
-      //         low: el[3],
-      //       };
-      //       low = Math.min(low, el[3]);
-      //       high = Math.max(high, el[2]);
-      //       crypto.push(frame);
-      //     });
-      //     // data = resData.prices;
-      //   }
-      //   setData(crypto);
-      //   setCurrentValue(crypto[crypto.length - 1].open);
-      //   console.log(crypto[0].value, crypto[1].value);
-      //   setTwentyFourHourValue(crypto[0].open);
-      //   setTimeFrameMax(high);
-      //   setTimeFrameLow(low);
     };
     getData();
   }, [coinId, timeFrame]);
 
-  // useEffect(() => {
-  //   fetch(
-  //     `https://api.coingecko.com/api/v3/coins/${coinId}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       const coinInfo = {
-  //         name: data?.name,
-  //         description: data?.description?.en,
-  //         url: data?.links?.homepage[0],
+  useEffect(() => {
+    if (coinInfo.symbol && individualPage) {
+      fetch(`https://price-api.crypto.com/price/v1/tokens?page=1&limit=500`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log({ data });
+          const findId = data.data.filter(
+            (el: { symbol: any }) => el.symbol.toLowerCase() === coinInfo.symbol
+          );
 
-  //         image: data?.image?.small,
-  //         score: data?.community_score,
-  //       };
-  //       setCoinInfo(coinInfo);
-  //     });
-  // }, [coinId]);
+          setCryptoId(findId[0].token_id);
+        });
+    }
+  }, [coinId, coinInfo, individualPage]);
+
+  useEffect(() => {
+    if (cryptoId > 0 && individualPage) {
+      fetch(`https://price-api.crypto.com/market/v2/token/${cryptoId}/news`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setNews(data);
+        });
+    }
+  }, [cryptoId, individualPage]);
 
   useEffect(() => {
     console.log(timeFrameLow, timeFrameMax);
@@ -293,7 +281,10 @@ const ChartComponent = (props: any) => {
     { query: 365, value: "Y" },
     { query: "max", value: "All" },
   ];
-
+  const dateParse = (date: string) => {
+    const frame = date.slice(0, 10).split("-");
+    return `${frame[1]}/${frame[2]}/${frame[0]}`;
+  };
   return (
     <Box p="40px 0">
       {coinInfo.name.length > 0 && chartContainerRef ? (
@@ -432,22 +423,143 @@ const ChartComponent = (props: any) => {
               </Text>
             </Box>
           </Box>
-          {individualPage && (
-            <Box>
-              <Text fontWeight="bold">{coinInfo.name}</Text>
-              <Box
-                maxH="200px"
-                dangerouslySetInnerHTML={{ __html: coinInfo.description }}
-                textOverflow="ellipsis"
-                overflow="hidden"
-                whiteSpace="break-spaces"
-                lineHeight="1.5"
-                height="calc(15px * 13)"
-              />
-            </Box>
-          )}
         </>
       ) : null}
+
+      <Tabs width={{ base: "100%", md: "40%" }} pt="40px">
+        <TabList borderBottom="unset" pb="20px" gap="40px">
+          <Tab fontSize="24px" fontWeight="700" pb="20px">
+            Bio
+          </Tab>
+          {news?.articles.length > 0 ? (
+            <Tab fontSize="24px" fontWeight="700" pb="20px">
+              News
+            </Tab>
+          ) : null}
+          {news?.videos.length > 0 ? (
+            <Tab fontSize="24px" fontWeight="700" pb="20px">
+              Videos
+            </Tab>
+          ) : null}
+        </TabList>
+        <TabPanels>
+          <TabPanel p="0">
+            {individualPage && (
+              <Box>
+                {/* <Text fontSize="32px" fontWeight="700" pb="20px">
+                  {coinInfo.name}
+                </Text> */}
+                <Box position="relative">
+                  <Box
+                    maxH={!viewAllCoin ? "500px" : "100%"}
+                    dangerouslySetInnerHTML={{ __html: coinInfo.description }}
+                    textOverflow="ellipsis"
+                    overflow="hidden"
+                    whiteSpace="break-spaces"
+                    lineHeight="1.5"
+                    transition="all .3s ease-in-out"
+                    height={
+                      !viewAllCoin
+                        ? { base: "calc(15px * 21)", md: "100%" }
+                        : "100%"
+                    }
+                  />
+                  <Box
+                    position="absolute"
+                    bottom="0"
+                    background={
+                      !viewAllCoin
+                        ? colorMode === "light"
+                          ? "linear-gradient(rgba(255, 255, 255, 0) 30%, rgb(255, 255, 255) 100%)"
+                          : "linear-gradient(rgba(8, 28, 59, 0) 30%, rgb(8, 28, 59) 100%)"
+                        : "unset"
+                    }
+                    h="150px"
+                    w="100%"
+                  />
+                </Box>
+                <Button onClick={() => setViewAllCoin(!viewAllCoin)} mt="20px">
+                  {!viewAllCoin ? "View More" : "View Less"}
+                </Button>
+              </Box>
+            )}
+          </TabPanel>
+          <TabPanel padding="0">
+            {news?.articles.length > 0 && individualPage ? (
+              <Stack gap="40px">
+                {news.articles.map((el: any) => (
+                  <Stack
+                    flexDir={{ base: "column" }}
+                    gap="20px"
+                    key={el.link}
+                    margin="0 !important"
+                    justifyContent="space-between"
+                    w="100%"
+                  >
+                    <Box
+                      backgroundImage={`url("${el.thumbnail}")`}
+                      backgroundSize="cover"
+                      sx={{
+                        aspectRatio: "16/9",
+                      }}
+                      w="100%"
+                      margin="0 !important"
+                    />
+                    <VStack
+                      width="100%"
+                      justifyContent="center"
+                      alignItems="flex-start"
+                    >
+                      <Link href={el.link} isExternal>
+                        <Text fontSize="24px" lineHeight="1.5" fontWeight="700">
+                          {el.title.replace(/[^a-zA-Z ]/g, "")}
+                          <span
+                            style={{
+                              display: "inline-block",
+                              marginLeft: "8px",
+                              lineHeight: "1.5",
+                            }}
+                          >
+                            <BiLinkExternal size={24} fill="#4983c7" />
+                          </span>
+                        </Text>
+                        <Text fontWeight="bold">
+                          {dateParse(el.publication_time)}
+                        </Text>
+                      </Link>
+                      <Text lineHeight="1.5" fontSize="18px" maxW="100%">
+                        {el.description}
+                      </Text>
+                    </VStack>
+                  </Stack>
+                ))}
+              </Stack>
+            ) : null}
+          </TabPanel>
+          <TabPanel padding="0">
+            {news?.videos.length > 0 && individualPage ? (
+              <Stack gap="40px">
+                {news.videos.map((el: any) => (
+                  <VStack key={el.id}>
+                    <Box
+                      as="iframe"
+                      src={`https://www.youtube.com/embed/${el.id}`}
+                      width="100%"
+                      allow="autoplay"
+                      sx={{
+                        aspectRatio: "16/9",
+                      }}
+                    />
+                    <Text fontSize="24px" lineHeight="1.5" fontWeight="700">
+                      {el.title}
+                    </Text>
+                  </VStack>
+                ))}
+              </Stack>
+            ) : null}
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Box>
   );
 };
