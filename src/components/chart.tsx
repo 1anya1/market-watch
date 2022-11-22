1c470ad089b7677ad015f7f4e370f883b733425e
Anna Filatova <filatovaa@yahoo.com>
2022-11-21 23:48:06 -0800

    added simple currency converter"

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -4,6 +4,7 @@ import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 import { RiSettings3Fill } from "react-icons/ri";
 import { NumericFormat } from "react-number-format";
 import { BiLinkExternal } from "react-icons/bi";
+
 import {
   Box,
   Button,
@@ -23,6 +24,12 @@ import {
   Tab,
   TabPanel,
   TabPanels,
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  InputLeftAddon,
+  InputGroup,
 } from "@chakra-ui/react";
 import Bitcoin from "../white/btc.svg";
 import BitcoinLightMode from "../black/btc.svg";
@@ -45,8 +52,8 @@ const ChartComponent = (props: any) => {
   const [timeFrame, setTimeFrame] = useState<number | string>(1);
   const [currentValue, setCurrentValue] = useState<number>(0);
   const [twentyFourHourValue, setTwentyFourHourValue] = useState(0);
-  const [timeFrameMax, setTimeFrameMax] = useState(0);
-  const [timeFrameLow, setTimeFrameLow] = useState(0);
+  const [timeFrameMax, setTimeFrameMax] = useState(1);
+  const [timeFrameLow, setTimeFrameLow] = useState(1);
   const [cryptoId, setCryptoId] = useState(0);
   const [viewAllCoin, setViewAllCoin] = useState(false);
   const [news, setNews] = useState<any>({
@@ -56,17 +63,21 @@ const ChartComponent = (props: any) => {
     videos: [],
     total: 0,
   });
-
-  const [coinInfo, setCoinInfo] = useState({
+  const [cryptoExchange, setCryptoExchange] = useState(1);
+  const [currencyExchange, setCurrencyExchange] = useState(0);
+  const [coinInfo, setCoinInfo] = useState<any>({
     name: "",
     description: "",
     url: "",
     image: "",
     score: 0,
     symbol: "",
+    currentPrice: {},
   });
   const { colorMode } = useColorMode();
-
+  useEffect(() => {
+    setCurrencyExchange(coinInfo.currentPrice.usd);
+  }, [coinInfo]);
   useEffect(() => {
     const getData = async () => {
       let crypto: any[] = [];
@@ -103,7 +114,7 @@ const ChartComponent = (props: any) => {
             name: data?.name,
             description: data?.description?.en,
             url: data?.links?.homepage[0],
-
+            currentPrice: data.market_data.current_price,
             image: data?.image?.small,
             score: data?.community_score,
             symbol: data?.symbol,
@@ -285,14 +296,24 @@ const ChartComponent = (props: any) => {
     const frame = date.slice(0, 10).split("-");
     return `${frame[1]}/${frame[2]}/${frame[0]}`;
   };
-  console.log(news);
+  const handleChangeCrypto = (event: any) => {
+    const value = event.target.value;
+    setCryptoExchange(value);
+    setCurrencyExchange(value * coinInfo.currentPrice.usd);
+  };
+
+  const handleChangeExchange = (event: any) => {
+    const value = event.target.value;
+    setCurrencyExchange(value);
+    setCryptoExchange(value / coinInfo.currentPrice.usd);
+  };
+
   return (
     <Box p="40px 0">
       {coinInfo.name.length > 0 && chartContainerRef ? (
         <>
-      
           <Box
-          borderRadius='11px'
+            borderRadius="11px"
             padding=" 10px 14px 40px 14px"
             backgroundColor={colorMode === "light" ? "#f5f6fa" : "#133364"}
             position="relative"
@@ -428,165 +449,220 @@ const ChartComponent = (props: any) => {
           </Box>
         </>
       ) : null}
-      {individualPage ? (
-        <Tabs width={{ base: "100%", md: "40%" }} pt="40px">
-          <TabList
-            borderBottom="unset"
-            pb="20px"
-            gap={{ base: "20px", md: "28px" }}
-          >
-            {coinInfo?.description ? (
-              <Tab
-                fontSize={{ base: "16px", md: "20px" }}
-                fontWeight="700"
-                pb="20px"
-              >
-                Bio
-              </Tab>
-            ) : null}
-            {news?.articles.length > 0 ? (
-              <Tab
-                fontSize={{ base: "16px", md: "20px" }}
-                fontWeight="700"
-                pb="20px"
-              >
-                News
-              </Tab>
-            ) : null}
-            {news?.videos.length > 0 ? (
-              <Tab
-                fontSize={{ base: "16px", md: "20px" }}
-                fontWeight="700"
-                pb="20px"
-              >
-                Videos
-              </Tab>
-            ) : null}
-          </TabList>
-          <TabPanels>
-            {individualPage && coinInfo?.description && (
-              <TabPanel p="0">
-                <Box>
-                  <Box position="relative">
-                    <Box
-                      maxH={!viewAllCoin ? "500px" : "100%"}
-                      dangerouslySetInnerHTML={{ __html: coinInfo.description }}
-                      textOverflow="ellipsis"
-                      overflow="hidden"
-                      whiteSpace="break-spaces"
-                      lineHeight="1.5"
-                      transition="all .3s ease-in-out"
-                      height={
-                        !viewAllCoin
-                          ? { base: "calc(15px * 21)", md: "100%" }
-                          : "100%"
-                      }
-                    />
-                    <Box
-                      position="absolute"
-                      bottom="0"
-                      background={
-                        !viewAllCoin
-                          ? colorMode === "light"
-                            ? "linear-gradient(rgba(255, 255, 255, 0) 30%, rgb(255, 255, 255) 100%)"
-                            : "linear-gradient(rgba(8, 28, 59, 0) 30%, rgb(8, 28, 59) 100%)"
-                          : "unset"
-                      }
-                      h="150px"
-                      w="100%"
-                    />
-                  </Box>
-
-                  <Button
-                    onClick={() => setViewAllCoin(!viewAllCoin)}
-                    mt="20px"
-                  >
-                    {!viewAllCoin ? "View More" : "View Less"}
-                  </Button>
-                </Box>
-              </TabPanel>
-            )}
-            {news?.articles.length > 0 && individualPage && (
-              <TabPanel padding="0">
-                <Stack gap="40px">
-                  {news.articles.map((el: any) => (
-                    <Stack
-                      flexDir={{ base: "column" }}
-                      gap="20px"
-                      key={el.link}
-                      margin="0 !important"
-                      justifyContent="space-between"
-                      w="100%"
-                    >
+      <Stack
+        flexDirection={{ base: "column", lg: "row" }}
+        columnGap={{base:'46px', xl:"100px"}}
+        rowGap='20px'
+        pt="40px"
+      >
+        {individualPage ? (
+          <Tabs width="100%">
+            <TabList
+              borderBottom="unset"
+              pb="20px"
+              gap={{ base: "20px", md: "28px" }}
+            >
+              {coinInfo?.description ? (
+                <Tab
+                  fontSize={{ base: "16px", md: "20px" }}
+                  fontWeight="700"
+                  pb="20px"
+                >
+                  Bio
+                </Tab>
+              ) : null}
+              {news?.articles.length > 0 ? (
+                <Tab
+                  fontSize={{ base: "16px", md: "20px" }}
+                  fontWeight="700"
+                  pb="20px"
+                >
+                  News
+                </Tab>
+              ) : null}
+            </TabList>
+            <TabPanels>
+              {individualPage && coinInfo?.description && (
+                <TabPanel p="0">
+                  <Box>
+                    <Box position="relative">
                       <Box
-                        backgroundImage={`url("${el.thumbnail}")`}
-                        backgroundSize="cover"
-                        sx={{
-                          aspectRatio: "16/9",
+                        maxH={!viewAllCoin ? "500px" : "100%"}
+                        dangerouslySetInnerHTML={{
+                          __html: coinInfo.description,
                         }}
+                        textOverflow="ellipsis"
+                        overflow="hidden"
+                        whiteSpace="break-spaces"
+                        lineHeight="1.5"
+                        transition="all .3s ease-in-out"
+                        height={
+                          !viewAllCoin
+                            ? { base: "calc(15px * 21)", md: "100%" }
+                            : "100%"
+                        }
+                      />
+                      <Box
+                        position="absolute"
+                        bottom="0"
+                        background={
+                          !viewAllCoin
+                            ? colorMode === "light"
+                              ? "linear-gradient(rgba(255, 255, 255, 0) 30%, rgb(255, 255, 255) 100%)"
+                              : "linear-gradient(rgba(8, 28, 59, 0) 30%, rgb(8, 28, 59) 100%)"
+                            : "unset"
+                        }
+                        h="150px"
                         w="100%"
+                      />
+                    </Box>
+
+                    <Button
+                      onClick={() => setViewAllCoin(!viewAllCoin)}
+                      mt="20px"
+                    >
+                      {!viewAllCoin ? "View More" : "View Less"}
+                    </Button>
+                  </Box>
+                </TabPanel>
+              )}
+              {news?.articles.length > 0 && individualPage && (
+                <TabPanel padding="0">
+                  <Stack gap="40px">
+                    {news.articles.map((el: any) => (
+                      <Stack
+                        flexDir={{ base: "column" }}
+                        gap="20px"
+                        key={el.link}
                         margin="0 !important"
-                      />
-                      <VStack
-                        width="100%"
-                        justifyContent="center"
-                        alignItems="flex-start"
+                        justifyContent="space-between"
+                        w="100%"
                       >
-                        <Link href={el.link} isExternal>
-                          <Text
-                            fontSize="24px"
-                            lineHeight="1.5"
-                            fontWeight="700"
-                          >
-                            {el.title.replace(/[^a-zA-Z ]/g, "")}
-                            <span
-                              style={{
-                                display: "inline-block",
-                                marginLeft: "8px",
-                                lineHeight: "1.5",
-                              }}
+                        <Box
+                          backgroundImage={`url("${el.thumbnail}")`}
+                          backgroundSize="cover"
+                          sx={{
+                            aspectRatio: "16/9",
+                          }}
+                          w="100%"
+                          margin="0 !important"
+                        />
+                        <VStack
+                          width="100%"
+                          justifyContent="center"
+                          alignItems="flex-start"
+                        >
+                          <Link href={el.link} isExternal>
+                            <Text
+                              fontSize="24px"
+                              lineHeight="1.5"
+                              fontWeight="700"
                             >
-                              <BiLinkExternal size={24} fill="#4983c7" />
-                            </span>
+                              {el.title.replace(/[^a-zA-Z ]/g, "")}
+                              <span
+                                style={{
+                                  display: "inline-block",
+                                  marginLeft: "8px",
+                                  lineHeight: "1.5",
+                                }}
+                              >
+                                <BiLinkExternal size={24} fill="#4983c7" />
+                              </span>
+                            </Text>
+                            <Text fontWeight="bold">
+                              {dateParse(el.publication_time)}
+                            </Text>
+                          </Link>
+                          <Text lineHeight="1.5" fontSize="18px" maxW="100%">
+                            {el.description}
                           </Text>
-                          <Text fontWeight="bold">
-                            {dateParse(el.publication_time)}
-                          </Text>
-                        </Link>
-                        <Text lineHeight="1.5" fontSize="18px" maxW="100%">
-                          {el.description}
-                        </Text>
-                      </VStack>
-                    </Stack>
-                  ))}
-                </Stack>
-              </TabPanel>
-            )}
-            {news?.videos.length > 0 && individualPage && (
-              <TabPanel padding="0">
-                <Stack gap="40px">
-                  {news.videos.map((el: any) => (
-                    <VStack key={el.id}>
-                      <Box
-                        as="iframe"
-                        src={`https://www.youtube.com/embed/${el.id}`}
-                        width="100%"
-                        allow="autoplay"
-                        sx={{
-                          aspectRatio: "16/9",
-                        }}
-                      />
-                      <Text fontSize="24px" lineHeight="1.5" fontWeight="700">
-                        {el.title}
-                      </Text>
-                    </VStack>
-                  ))}
-                </Stack>
-              </TabPanel>
-            )}
-          </TabPanels>
-        </Tabs>
-      ) : null}
+                        </VStack>
+                      </Stack>
+                    ))}
+                  </Stack>
+                </TabPanel>
+              )}
+            </TabPanels>
+          </Tabs>
+        ) : null}
+        {coinInfo.symbol.length > 0 && (
+          <Box
+            width="100%"
+            bg="#133364"
+            padding="20px"
+            borderRadius="11px"
+            h="max-content"
+          >
+            <Text fontSize="20px" fontWeight="700" pb="20px">
+              Currency Converter
+            </Text>
+
+            <InputGroup mb="10px">
+              <InputLeftAddon>{coinInfo.symbol.toUpperCase()} </InputLeftAddon>
+              <Input
+                type="number"
+                onChange={handleChangeCrypto}
+                value={cryptoExchange}
+              />
+            </InputGroup>
+            <InputGroup>
+              <InputLeftAddon>USD</InputLeftAddon>
+              <Input
+                type="number"
+                onChange={handleChangeExchange}
+                value={currencyExchange}
+              />
+            </InputGroup>
+
+            <Box fontSize="18px" pt="20px">
+              <span>
+                1{coinInfo.symbol.toUpperCase()} ={" "}
+                <NumericFormat
+                  value={coinInfo.currentPrice.usd}
+                  suffix=" USD"
+                  displayType="text"
+                  thousandSeparator=","
+                />
+              </span>
+            </Box>
+            <Text fontSize="12px">
+              This is not real time data. To use for approximation only*
+            </Text>
+          </Box>
+        )}
+      </Stack>
+
+      {news?.videos.length > 0 && individualPage && (
+        <>
+          <Text fontSize="20px" fontWeight="700" pb="20px" mt="40px">
+            Videos
+          </Text>
+          <Box overflow="scroll">
+            <HStack
+              gap="20px"
+              overflow="hidden"
+              width="max-content"
+              alignItems="flex-start"
+            >
+              {news.videos.map((el: any) => (
+                <VStack key={el.id} width={{ base: "250px", md: "300px" }}>
+                  <Box
+                    as="iframe"
+                    src={`https://www.youtube.com/embed/${el.id}`}
+                    width="100%"
+                    sx={{
+                      aspectRatio: "16/9",
+                    }}
+                  />
+                  <Text fontSize="18px" lineHeight="1.5" fontWeight="700">
+                    {el.title}
+                  </Text>
+                </VStack>
+              ))}
+            </HStack>
+          </Box>
+        </>
+      )}
     </Box>
   );
 };
