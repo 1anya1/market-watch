d401a509590c2ceac3e45e33c968c0c12c0b395f
Anna Filatova <filatovaa@yahoo.com>
2024-11-20 20:28:08 -0800

    updated next config to include updated api for images

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ module.exports = {
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
   },
   images: {
-    domains: ['assets.coingecko.com'],
+    domains: ['assets.coingecko.com','coin-images.coingecko.com'],
     minimumCacheTTL: 60,
 },
 };
